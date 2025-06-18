f5e13a183421dafc9111bff4cbc16c160511eac3
waiyee1024 <waiyee.24@ichat.sp.edu.sg>
2025-06-18 17:44:16 +0800

    With_frontend

--- frontend/src/api/userApi.jsx
+++ frontend/src/api/userApi.jsx
@@ -4,8 +4,9 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api', // Your backend URL
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 });
+
 
 // Add token to requests automatically
 API.interceptors.request.use((config) => {
