8b2256ed4f986e2d0843e5901d3bce04229fd535
PerynnNeo <perynnneo.24@ichat.sp.edu.sg>
2025-06-22 03:35:59 +0800

    tfbfbdf

--- frontend/src/components/RiskSimulator.jsx
+++ frontend/src/components/RiskSimulator.jsx
@@ -7,7 +7,7 @@ import { getDashboardRiskData } from '../api/dashboardApi';
 import '../styles/styles.css';
 import '../styles/dashboard.css';
 import { PDFDownloadLink } from '@react-pdf/renderer';
-import RiskPlanPdf from './RiskPlanPdf';
+import LifestylePlanPdf from './LifestylePlanPdf';
 
 const CancerRiskHelpers = {
   getRiskColors: (level, colors) => {
@@ -536,7 +536,7 @@ const RiskSimulator = () => {
         <div className="risk-actions">
           <PDFDownloadLink
             document={
-              <RiskPlanPdf
+              <LifestylePlanPdf
                 currentRiskScore={currentRiskScore}
                 simulatedRiskScore={simulatedRiskScore}
                 modifiableFactors={modifiableFactors}
